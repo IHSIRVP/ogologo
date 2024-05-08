c6ee42b1be915be398919b6dc4fb3822a836e546
Priyanshusinghal123 <priyanshu.singhal@plaksha.edu.in>
2024-05-09 02:23:11 +0530

    Unet Residual Block added

--- model-architecture/diffusion.py
+++ model-architecture/diffusion.py
@@ -20,3 +20,19 @@ class TimeEmbedding(nn.Module):
         
         # (1, 1280) -> (1, 1280)
         x = self.linear_2(x)
+
+
+class UNET_ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, n_time=1280):
+        super().__init__()
+        self.groupnorm_feature = nn.GroupNorm(32, in_channels)
+        self.conv_feature = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.linear_time = nn.Linear(n_time, out_channels)
+
+        self.groupnorm_merged = nn.GroupNorm(32, out_channels)
+        self.conv_merged = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+
+        if in_channels == out_channels:
+            self.residual_layer = nn.Identity()
+        else:
+            self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0)
