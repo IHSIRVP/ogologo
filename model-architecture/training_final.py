f681befbbb12d566300b61b696291a5da9c0ed8f
Meenal Singh <sanidhya.singh@plaksha.edu.in>
2024-05-11 14:58:11 +0530

    add explanatory commit

--- model-architecture/training_final.py
+++ model-architecture/training_final.py
@@ -100,7 +100,7 @@ def get_time_embedding(timestep):
     # Shape: (160,)
     freqs = torch.pow(10000, -torch.arange(start=0, end=160, dtype=torch.float32) / 160) 
     # Shape: (1, 160)
-    x = torch.tensor([timestep], dtype=torch.float32)[:, None] * freqs[None]
+    x = torch.tensor([timestep], dtype=torch.float32)[:, None] * freqs[None]# temporal scaling
     # Shape: (1, 160 * 2)
     return torch.cat([torch.cos(x), torch.sin(x)], dim=-1)
 
